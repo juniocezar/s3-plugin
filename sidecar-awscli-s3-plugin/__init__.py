ec24e18fdfa8f776e11b7756a8a37f4828382981
Junio <jcezar@cyral.com>
2021-03-22 21:25:42 -0300

    Get base sidecar profile from configuration

--- sidecar-awscli-s3-plugin/__init__.py
+++ sidecar-awscli-s3-plugin/__init__.py
@@ -1,15 +1,17 @@
 import os
 import warnings
 
-
 PROXY = 'proxy'
 COMMANDS = ['s3', 's3api']
 VERIFY_SSL = 'verify_ssl'
 CA_BUNDLE = 'ca_bundle'
+SIDECAR_CREDS = 'sidecar_credentials'
 
 def str2bool(value):
     return str(value).lower() in ['1', 'yes', 'y', 'true', 'on']
 
+def get_sidecar_credentials_source(profile, command):
+    return profile.get(command, {}).get(SIDECAR_CREDS)
 
 def get_ca_bundle_from_profile(profile, command):
     return profile.get(command, {}).get(CA_BUNDLE)
@@ -70,7 +72,7 @@ def set_proxy_from_profile(parsed_args, **kwargs):
 
     if (service_proxy is not None) and (command in COMMANDS):
         print('[plugin] Detected command: ' + command)
-        print('[plugin] Using Sidecar: ' + service_proxy + "\n")
+        print('[plugin] Using Sidecar: ' + service_proxy)
 
         os.environ['http_proxy'] = service_proxy
         os.environ['https_proxy'] = service_proxy
@@ -78,7 +80,30 @@ def set_proxy_from_profile(parsed_args, **kwargs):
         os.environ['HTTPS_PROXY'] = service_proxy
         os.environ['NO_PROXY'] = '169.254.169.254'
 
+
+def set_sidecar_credentials(parsed_args, **kwargs):
+    session = kwargs['session']
+    command = parsed_args.command
+
+    if command not in ["s3", "s3api"]:
+        return
+
+    src_profile = get_sidecar_credentials_source(session.get_scoped_config(), command)
+    if src_profile == "" or src_profile == None:
+        return
+
+    print("[plugin] Using sidecar profile: " + src_profile + "\n")
+    src_profile_data = session._config['profiles'][src_profile]
+    if src_profile_data == "" or src_profile_data == None:
+        print("[plugin] Source profile has no associated credentials")
+        return
+    
+    os.environ['AWS_ACCESS_KEY_ID'] = src_profile_data['aws_access_key_id']
+    os.environ['AWS_SECRET_ACCESS_KEY'] = src_profile_data['aws_secret_access_key']
+
+
 def awscli_initialize(cli):
     cli.register('top-level-args-parsed', set_proxy_from_profile)
     cli.register('top-level-args-parsed', set_verify_from_profile)
     cli.register('top-level-args-parsed', set_ca_bundle_from_profile)
+    cli.register('top-level-args-parsed', set_sidecar_credentials)
